5c901e7b4f1596db7d9827ca3352dfe7a1bb5fc2
murum <cr222cs@student.lnu.se>
2013-05-18 11:48:59 +0200

    roles needed in archive view

--- wp-content/themes/utvecklarkollektivet/archive-projekt.php
+++ wp-content/themes/utvecklarkollektivet/archive-projekt.php
@@ -8,8 +8,21 @@ get_header(); ?>
 				<div class="span8 uk-projekt">
 					<a href="<?php the_permalink(); ?>"><h2><?php echo the_title(); ?></h2></a>
 					<p><?php echo the_excerpt(); ?></p>
+
+					<?php $roles_needed = simple_fields_fieldgroup("roles_needed"); ?>
+
+					<?php // Om det inte finns några roller som behövs. ?>
+					<?php if(sizeof($roles_needed) > 0) : ?>
+						<h4>Roller som behövs</h4>
+						<ul class="uk-project-roles-needed">
+							<?php foreach($roles_needed as $role) : ?>
+								<li class="uk-project-roles-needed-role"><?php echo $role; ?></li>
+							<?php endforeach; ?>
+						</ul>
+					<?php endif; ?>
+					
 					<?php $membersInProjekt = simple_fields_fieldgroup("medlemmar"); ?>
-					<h3>Medlemmar i projektet</h3>
+					<h4>Medlemmar i projektet</h4>
 					<ul class="uk-projekt-medlemmar">
 						<?php foreach($membersInProjekt as $member) : ?>
 							<li class="uk-projekt-medlemmar-medlem"><?php echo $member['medlem_fornamn']." ".$member['medlem_efternamn']." - ".$member['medlem_roll']; ?></li>
