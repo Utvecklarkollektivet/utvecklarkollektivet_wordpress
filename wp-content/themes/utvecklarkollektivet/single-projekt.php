7588ce9596b691837e8ef332a6579bb30fe810ec
Christoffer Artmann <Artgaard@gmail.com>
2013-07-03 23:52:24 +0200

    Updated project page to show the lastest commits for each repository

--- wp-content/themes/utvecklarkollektivet/single-projekt.php
+++ wp-content/themes/utvecklarkollektivet/single-projekt.php
@@ -19,6 +19,7 @@ get_header(); ?>
 					</ul>
 				<?php endif;?>
 
+
 				
 				<?php $membersInProjekt = simple_fields_fieldgroup("medlemmar"); ?>
 				<h3>Medlemmar i projektet</h3>
@@ -28,6 +29,16 @@ get_header(); ?>
 					<?php endforeach; ?>
 				</ul>
 				<?php $github_link = simple_fields_fieldgroup("github_link"); ?>
+				<h4>Github</h4>
+				<?php
+					if(strlen($github_link) > 0) {
+						$user = wp_get_current_user();
+						if($user) {
+							$api = new GHAPI($user);
+							$api->renderLatestCommits();
+						}	
+					}
+				?>
 				<h4>Repo</h4>				
 				<a class="uk-project-github" href="<?php echo $github_link; ?>"><span class="icon-github-sign icon-4x"></span></a>
 			</div>
